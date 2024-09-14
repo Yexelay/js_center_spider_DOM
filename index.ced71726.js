dfcaa4766aea5a337d11ceedce00f66ca4410671
Oleksii Kudriavtsev <Lesha61945@gmail.com>
2024-09-14 10:36:13 +0300

    make build

---
+++ index.ced71726.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=t.clientWidth,c=t.clientHeight,i=e.clientWidth,o=e.clientHeight;e.style.position="absolute",e.style.left="".concat((l-i)/2,"px"),e.style.top="".concat((c-o)/2,"px");
+//# sourceMappingURL=index.ced71726.js.map
